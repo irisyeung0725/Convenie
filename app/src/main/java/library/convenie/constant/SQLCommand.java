2b9c855a71da0db573e261da0449f477992b8379
Iris <ciciyeung96@gmail.com>
2019-04-06 16:56:07 -0400

    Modified QUERY_EXPIRE

--- app/src/main/java/library/convenie/constant/SQLCommand.java
+++ app/src/main/java/library/convenie/constant/SQLCommand.java
@@ -12,7 +12,7 @@ public abstract class SQLCommand {
     public static String QUERY_VENDOR = "select Vendor_Name, Vendor_Type from Vendor";
     public static String QUERY_TRANSACTION = "select count(InvoiceID), Sales_Date from Invoice group by Sales_Date";
     public static String QUERY_ITEMS = "select Items_Name, Item_QOH from Items order by Item_QOH";
-    public static String QUERY_EXPIRE = "select Items_Name, Item_QOH from Items order by Expire_Date";
+    public static String QUERY_EXPIRE = "select Item_Category, Items_Name, cast((julianday(date(Expire_date)) - julianday('2017-07-20')) as int) as \"Days to expire\" from Items where \"Days to expire\" > 0 order by Item_Category";
     public static String QUERY_SALE = "select sum(Line_Qty * Line_Price) as Total_Sales, Sales_Date from InvoiceLines, " +
             "Invoice where InvoiceLines.InvoiceID = Invoice.InvoiceID group by Invoice.Sales_Date";
 }
